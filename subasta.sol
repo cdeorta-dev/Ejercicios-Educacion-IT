c1a628a7e59d338087ebf6e32e638b5529d9b366
cdeorta-dev <cdeorta.dev@gmail.com>
2025-01-16 14:34:45 -0300

    cambie todo por mapping

--- subasta.sol
+++ subasta.sol
@@ -7,62 +7,80 @@ laa subastasolamente se le puede parar el owner
 2 va a ver una tupla o variable mayor apostador que se va a inicializar cuando se inicialize la subasta, que lo va arrancar el subastardor el subastador va a ser el primer mayor 
 apostador con lo que inicie en la apuestsa 
 otra funcion que va  a estar dentro de depositar que va a ser setear mayor apostador
+el owner no puede participar
+
 */
 
 contract subasta {
-    event EtherRecibido(address remitente, uint256 monto);
+    event EtherRecibido(address remitente, uint256 monto,uint256 total);
     address private owner;
     struct Person{
         address payable direccion;
         uint256 apuesta;
     }
-    Person[] public listaApostadores;
-    Person public mayorApostador;
+    mapping (address  => uint256) public totalinversion;
+    address payable[] public listaApostadores;
+     address payable mayorApostador;
     constructor() payable {
         owner=msg.sender;
-        mayorApostador.direccion= payable(msg.sender);
-        mayorApostador.apuesta= msg.value;
+        mayorApostador= payable(msg.sender);
+         totalinversion[msg.sender]= msg.value;
     }
     modifier onlyOwner(){
         require(owner==msg.sender,"no eres el owner");
         _;
     }
-    function transferEtherOwner() external onlyOwner {
+    function transferEtherOwnerGanador() external onlyOwner {
         address payable _to;
         _to = payable (msg.sender);
-        if( _to.send(mayorApostador.apuesta) == false ){
+        if( _to.send(totalinversion[mayorApostador]) == false ){
             revert("fallo el envio");
         }
          devolverApuestas();
         
     }
-
-    receive() external payable {
-        if(mayorApostador.apuesta<msg.value){
-            mayorApostador.direccion=payable(msg.sender);
-            mayorApostador.apuesta= msg.value;
+     function transferEtherOwner() external onlyOwner {
+        address payable _to;
+        _to = payable (msg.sender);
+        if( _to.send(address(this).balance) == false ){
+            revert("fallo el envio");
         }
+        
+        
+    }
+//el owner no puede participar
+    receive() external payable {
+ 
+
+
         require(msg.value >= 1000, "El monto debe ser al menos 1000 wei"); 
         require(msg.value % 100 == 0, "El monto debe ser multiplo de 100 wei"); 
-        emit EtherRecibido(msg.sender, msg.value);
-    }
-
-
-   function GetMayorApostador() external view returns (Person memory) {
+     
+        listaApostadores.push(payable(msg.sender)) ;
+         totalinversion[msg.sender]+= msg.value;
+       
+  if(totalinversion[mayorApostador]<totalinversion[msg.sender]){
+            mayorApostador=payable(msg.sender);
         
-        Person memory persona;
-        for (uint i = 0; i < listaApostadores.length; i++) 
-        {
-           if (persona.apuesta<listaApostadores[i].apuesta){
-                persona=listaApostadores[i];
-           }
         }
-        return persona;
+
+        emit EtherRecibido(msg.sender, msg.value,totalinversion[msg.sender]);
     }
+
+
+  
     
    function GetApostadores() external view returns (Person[] memory) {
-        
-        return listaApostadores;
+
+        Person[]  memory valorApostadores = new  Person[] (listaApostadores.length);
+       
+        for (uint256 i ;i< listaApostadores.length;i++ ){
+            Person memory aux;
+            aux.direccion = listaApostadores[i];
+            aux.apuesta = totalinversion[listaApostadores[i]];
+            valorApostadores[i]=aux;
+        }   
+        return valorApostadores;
     }
     //hay que devolver el 98% a cada participante
     function devolverApuestas() internal onlyOwner{
@@ -70,8 +88,8 @@ contract subasta {
      
         for (uint i = 0; i < listaApostadores.length; i++) 
         {
-         if(listaApostadores[i].direccion!= mayorApostador.direccion)
-         if(  listaApostadores[i].direccion.send( listaApostadores[i].apuesta* 98 / 100) == false ){
+         if(listaApostadores[i]!= mayorApostador)
+         if(  listaApostadores[i].send( totalinversion[listaApostadores[i]]* 98 / 100) == false ){
             revert("fallo el envio");
         }
         }
